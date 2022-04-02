312201e95c570934102f9540a94fb66ef97cdf70
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2022-04-02 18:52:28 +0900

    fix: url

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ async function copyToClipBoard() {
   const schedule = document.getElementById('schedule').value;
   await navigator.clipboard.writeText(schedule);
   alert("スケジュールをコピーしました。\n調整さんの「候補日程」に貼り付けてください。");
-  window.location = 'https://chouseisan.com/'
+  window.location = 'https://chouseisan.com/#tab2';
 }
 document.getElementById('copy').onclick = copyToClipBoard;
 window.onload = insertQuietTimeSchedule;
